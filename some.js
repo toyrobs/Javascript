537d23c61850b15b9e5f61a2a413f0550273678c
Toyrobs <toyinrobinson@LAPTOP-GS5G147A.localdomain>
2020-06-04 00:35:45 -0400

    New edits

--- some.js
+++ some.js
@@ -4,10 +4,16 @@ Without using the native “Array.prototype.some” method of JavaScript,
  compose a function titled “mySome” that will take in an array of elements and executes
   a callback function on each of those elements.
   */
-/*
-*/
-function mySome(arr,callback)
+
+function mySome(arr,callback,)
 {
-	callback(arr(i));
+ for(let i=0;i<arr.length;i++)
+	 {
+     	if(callback(arr[i]))
+     	{
+     		return true
+     	} 	
+      }
+      return false
 }
-myReduce([1,2,3], obj =>console.log(obj))
+console.log(mySome([1,2,3,4], element=>element %2 === 0))
